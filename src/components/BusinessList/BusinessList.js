e06c879ed7d2fdec6a176441a76970d63182d7df
tbadun <58403128+tbadun@users.noreply.github.com>
2020-08-27 09:51:42 -0400

    Part 2 complete
    prep for Yelp API

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
-const businesses = [<Business />,<Business />,<Business />,<Business />].map(item => <li>{item}</li>);
-
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                <ul>{businesses}</ul>
+                <ul>{this.props.businesses.map(business => {return <Business business={business}/>;})}</ul>
             </div>
         )
     }
