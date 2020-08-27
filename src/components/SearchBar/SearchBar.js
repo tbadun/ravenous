caea52518959f1b81d7bf9d2383f3d7b23d78a66
tbadun <58403128+tbadun@users.noreply.github.com>
2020-08-27 11:56:06 -0400

    Part 3 done
    Added visual feedback for sorting, stateless app updates stateful searchbar, simulated query

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,30 +1,66 @@
 import React from 'react';
 import './SearchBar.css';
 
-let sortByOptions = {
-    "Best Match": 'best_match',
-    "Rating": 'rating',
-    "Review Count": 'review_count'
-};
-
 class SearchBar extends React.Component {
-    renderSortByOptions() {
-        var sortByOptionValue = Object.keys(sortByOptions).map(sortByOption => {return sortByOptions[sortByOption]});
-        return <li key="sortByOptionValue">{sortByOptions}</li> // this was following instr but I feel like it makes more sense to have the map structure send back a list of multiple LI
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        };
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.sortByOptions = {
+            "Best Match": 'best_match',
+            "Highest Rated": 'rating',
+            "Review Count": 'review_count'
+        };
     }
+
+    getSortByClass(sortByOption) {
+        if (this.state.sortBy === sortByOption) {
+            return 'active';
+        }
+        return '';
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({sortBy: sortByOption});
+    }
+
+    handleTermChange(event) {
+        this.setState({term: event.target.value});
+    }
+
+    handleLocationChange(event) {
+        this.setState({location: event.target.value});
+    }
+
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term,this.state.location,this.state.sortBy);
+        event.preventDefault();
+    }
+
+    renderSortByOptions() {
+        var sortByOptionValue = Object.keys(this.sortByOptions).map(sortByOption => {return <li key={this.sortByOptions[sortByOption]} value={this.sortByOptions[sortByOption]} className={this.getSortByClass(this.sortByOptions[sortByOption])} onClick={this.handleSortByChange.bind(this,this.sortByOptions[sortByOption])}>{sortByOption}</li>} );
+        return sortByOptionValue
+    }
+
     render() {
         return (
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        {this.renderSortByOptions}
+                        {this.renderSortByOptions()}
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+                    <input placeholder="Where?" onChange={this.handleLocationChange}/>
                 </div>
-                <div className="SearchBar-submit">
+                <div className="SearchBar-submit" onClick={this.handleSearch}>
                     <a>Let's Go</a>
                 </div>
             </div>
